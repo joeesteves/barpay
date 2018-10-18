f61c1665e61398c7551844db94f16200d9354a9a
José Esteves <ajesteves@gmail.com>
2018-10-18 15:55:05 -0300

    add teamplace local dep

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Barpay.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mercado_pago, path: "../mercado_pago"}
+      {:mercado_pago, path: "../mercado_pago"},
+      {:teamplace, path: "../teamplace"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
