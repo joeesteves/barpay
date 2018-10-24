56765d809c8012e07bfdd5a09a968f192b0fad1d
José Esteves <ajesteves@gmail.com>
2018-10-24 11:54:19 -0300

    add post caso

--- lib/barpay.ex
+++ lib/barpay.ex
@@ -12,49 +12,43 @@ defmodule Barpay do
       :world
 
   """
-  def hello do
-    #Implement worker login
+  def start do
+    # Implement worker login
     # check teamplace every 5 minutes and find ordenes de pago
-    IO.puts "CALLING MP"
-    |> IO.puts
+    IO.puts(
+      "CALLING MP"
+      |> IO.puts()
+    )
+
     :timer.sleep(20000)
-    hello
+    start
   end
 
   def get_pending_docs do
     Application.get_env(:teamplace, :credentials)
-    |> IO.inspect
     |> Teamplace.get_data("reports", "despachos", %{Empresa: "PRUEBA39", FechaDesde: "2018-01-01"})
   end
 
   def create_link(title, description, amount) do
     MercadoPago.get_payment_link("TEST", "TEST", 1)
+  end
 
+  def post_link(transaccionid, link) do
+    Application.get_env(:teamplace, :credentials)
+    |> Teamplace.post_data("caso", create_caso(transaccionid, link))
+  end
+
+  defp create_caso(transaccionid, link) do
+    %{
+      Prioridad: "1",
+      Titulo: transaccionid,
+      Descripcion: link,
+      FechaComprobante: "2018-10-18",
+      Fecha: "2018-10-18",
+      PersonaIDPropietario: "15",
+      TransaccionTipoID: "CASOTEAMPLACE",
+      TransaccionSubtipoID: "CB_ASIGN_LINK"
+    }
+    |> Poison.encode!()
   end
 end
-
-
-# %{
-#   Prioridad:1,
-#   Descripcion: build_description(order.desc, order.user, order.machine.name),
-#   Titulo: order.title,
-#   FechaComprobante: order.date,
-#   Fecha: order.date,
-#   PersonaIDPropietario:"413",
-#   TransaccionTipoID: "CASOTEAMPLACE",  #IMPORTANTE
-#   TransaccionSubtipoID:"CB_ASIGN_LINK",  # CODIGO DEL BPM
-#   MaquinaCodigo: order.machine_id
-#   }
-#   |>Poison.encode!()
-
-# WORKING VERSION FROM POSTMAN
-# {
-#   "Prioridad": "1",
-#   "Descripcion": "prueba - prueba",
-#   "Titulo": "6250",
-#   "FechaComprobante": "2018-10-18",
-#   "Fecha": "2018-10-18",
-#   "PersonaIDPropietario":"15",
-#   "TransaccionTipoID": "CASOTEAMPLACE",
-#   "TransaccionSubtipoID":"CB_ASIGN_LINK"
-# }
