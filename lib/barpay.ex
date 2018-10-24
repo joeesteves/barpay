1fbb21040dedc72f70fb4035bf3f99aee24b0351
José Esteves <ajesteves@gmail.com>
2018-10-24 15:36:18 -0300

    Working Version

--- lib/barpay.ex
+++ lib/barpay.ex
@@ -24,18 +24,38 @@ defmodule Barpay do
     start
   end
 
+  def work do
+    get_pending_docs
+    |> Enum.each(fn %{"TOTAL" => total, "TRANSACCIONID" => id, "DOCUMENTO" => doc} ->
+      create_link("Link de pago despacho #{doc}", "", total)
+      |> post_link(id)
+[]
+      IO.puts("Se genero el link de pago para #{doc}")
+      :timer.seconds(5)
+      |> :timer.sleep
+
+    end)
+    IO.puts("No hay despachos pendientes para procesar.. ")
+    IO.puts("Proximo chequeo en 5 min... ")
+
+    :timer.minutes(5)
+    |> :timer.sleep
+
+  end
+
   def get_pending_docs do
     Application.get_env(:teamplace, :credentials)
-    |> Teamplace.get_data("reports", "despachos", %{Empresa: "PRUEBA39", FechaDesde: "2018-01-01"})
+    |> Teamplace.get_data("reports", "despachos", %{Empresa: "PRUEBA39", FechaDesde: "2018-08-01", SoloPendientes: 1})
   end
 
   def create_link(title, description, amount) do
-    MercadoPago.get_payment_link("TEST", "TEST", 1)
+    MercadoPago.get_payment_link(title, description, amount)
   end
 
-  def post_link(transaccionid, link) do
+  def post_link(link, transaccionid) do
     Application.get_env(:teamplace, :credentials)
     |> Teamplace.post_data("caso", create_caso(transaccionid, link))
+
   end
 
   defp create_caso(transaccionid, link) do
