874845e10ed2d55d959be8123ca365b52cdf46c3
José Esteves <ajesteves@gmail.com>
2018-11-02 15:14:56 -0300

    Change MercadoLibre and Teamplace deps to git

--- mix.exs
+++ mix.exs
@@ -22,8 +22,8 @@ defmodule Barpay.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mercado_pago, path: "../mercado_pago"},
-      {:teamplace, path: "../teamplace"},
+      {:mercado_pago, git: "https://github.com/ponyesteves/mercado_pago.git", tag: "v0.2.1"},
+      {:teamplace, git: "https://github.com/ponyesteves/teamplace.git", tag: "v0.1.0"},
       {:poison, "~> 3.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
