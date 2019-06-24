cca0be0ab683cbf14e27d4051342befea8dc7b0c
Jose Esteves <jose@ceibo.co>
2019-06-24 14:15:34 -0300

    wip

--- lib/barpay/cobranza.ex
+++ lib/barpay/cobranza.ex
@@ -2,6 +2,19 @@ defmodule Barpay.Cobranza do
   alias Teamplace.Cobranza
   alias Teamplace.Cobranza.{Banco, Otros, CtaCte, Cotizacion}
 
+  def parse_payment(payment_json) do
+    payment_decoded = Poison.decode!(payment_json)
+
+    total = payment_decoded["transaction_details"]["total_paid_amount"]
+    importe_neto = payment_decoded["transaction_details"]["net_received_amount"]
+
+    %{
+      cliente_codigo: extract_cliente_codigo(payment_decoded["description"]),
+      total: total,
+      importe_neto: importe_neto
+    }
+  end
+
   def create(cliente_codigo, total, importe_neto) do
     comision = Float.round(total - importe_neto, 2)
 
@@ -34,5 +47,9 @@ defmodule Barpay.Cobranza do
       _ ->
         IO.puts("Error en la creción o envío de la cobranza")
     end
+  end
+
+  defp extract_cliente_codigo(description) do
+    Regex.named_captures(~r/\*\*\s(?<value>[^\-]*)\s\-/, description)["value"]
   end
 end
